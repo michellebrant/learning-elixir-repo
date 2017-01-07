40a94ab563937e517cf729c2019c2cf7d9b508e4
Michelle Brant <michellebrant>
2017-01-07 13:22:44 -0500

    user creation working

--- new_app/web/controllers/registration_controller.ex
+++ new_app/web/controllers/registration_controller.ex
@@ -6,4 +6,31 @@ defmodule NewApp.RegistrationController do
     changeset = User.changeset(%User{})
     render conn, changeset: changeset
   end
+
+#   def create(conn, %{"user" => user_params}) do
+#   changeset = User.changeset(%User{}, user_params)
+
+#   case NewApp.Registration.create(changeset, NewApp.Repo) do
+#     {:ok, changeset} ->
+#       # sign in the user
+#     {:error, changeset} ->
+#       # show error message
+#   end
+# end
+def create(conn, %{"user" => user_params}) do
+  changeset = User.changeset(%User{}, user_params)
+
+  case NewApp.Registration.create(changeset, NewApp.Repo) do
+    {:ok, changeset} ->
+      conn
+      |> put_flash(:info, "Your account was created")
+      |> redirect(to: "/")
+    {:error, changeset} ->
+      conn
+      |> put_flash(:info, "Unable to create account")
+      |> render("new.html", changeset: changeset)
+  end
 end
+end
+
+
--- new_app/web/models/user.ex
+++ new_app/web/models/user.ex
@@ -2,19 +2,32 @@ defmodule NewApp.User do
   use NewApp.Web, :model
 
   schema "users" do
-    field :email, :string
-    field :crypted_password, :string
+  field :email, :string
+  field :crypted_password, :string
+  field :password, :string, virtual: true
+  timestamps
+end
 
-    timestamps()
-  end
 
+  @required_fields ~w(email password)
+  @optional_fields  ~w(crypted_password)
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:email, :crypted_password])
-    |> validate_required([:email, :crypted_password])
-    |> unique_constraint(:email)
-  end
+  # def changeset(struct, params \\ %{}) do
+  #   struct
+  #   |> cast(params, [:email, :crypted_password])
+  #   |> validate_required([:email, :crypted_password])
+  #   |> unique_constraint(:email)
+  # end
+
+def changeset(model, params \\ :empty) do
+  model
+  |> cast(params, @required_fields, @optional_fields)
+  |> unique_constraint(:email)
+  |> validate_format(:email, ~r/@/)
+  |> validate_length(:password, min: 5)
 end
+end
+
+
